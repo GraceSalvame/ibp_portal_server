0a2e9c388b3f4cef03576a15f1cb0895625dc81c
GraceSalvame <gracesalvz@gmail.com>
2024-04-18 16:26:12 +0800

    model edit

--- Model/Ibp_registration.cs
+++ Model/Ibp_registration.cs
@@ -1,27 +1,26 @@
 ﻿using System.ComponentModel.DataAnnotations.Schema;
 using System.ComponentModel.DataAnnotations;
-using System.Data;
 
 namespace ibp_portal_server.Model
 {
     public class Ibp_registration
     {
-        [Key]
-        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
-        public int Id { get; set; }
+            [Key]
+            [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+            public int id { get; set; }
 
-        [ForeignKey("Ibp")]
-        public int ibp_id { get; set; }
-        public Ibp Ibp { get; set; }
+            [ForeignKey("Ibp")]
+            public int ibp_id { get; set; }
+            public Ibp Ibp { get; set; }
 
-        [ForeignKey("User")]
-        public int user_id { get; set; }
-        public User User { get; set; }
+            [ForeignKey("User")]
+            public int user_id { get; set; }
+            public User User { get; set; }
 
-        [ForeignKey("Role")]
-        public int role_id { get; set; }
-        public Role Role { get; set; }
+            [ForeignKey("Role")]
+            public int role_id { get; set; }
+            public Role Role { get; set; }
 
-        public DateTime confirmed_date { get; set; }
+            public DateTime confirmed_date { get; set; }
+        }
     }
-}
---
+++ Model/Status.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace ibp_portal_server.Model
+{
+    public class Status
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int id { get; set; }
+
+        [Required]
+        [Column(TypeName = "varchar(50)")]
+        public string code { get; set; }
+
+        [Required]
+        [Column(TypeName = "varchar(100)")]
+        public string name { get; set; }
+
+        [Column(TypeName = "text")]
+        public string description { get; set; }
+    }
+}
