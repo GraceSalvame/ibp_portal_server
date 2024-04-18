d77c1257f4aeacac73fba871b6af8c8bd5f77b6a
GraceSalvame <gracesalvz@gmail.com>
2024-04-18 15:05:31 +0800

    add Models

---
+++ Model/Corporate_affiliate.cs
@@ -0,0 +1,32 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace ibp_portal_server.Model
+{
+    public class Corporate_affiliate
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int id { get; set; }
+
+        [ForeignKey("Ibp")]
+        public int ibp_id { get; set; }
+        public Ibp Ibp { get; set; }
+
+        [Required]
+        [Column(TypeName = "varchar(100)")]
+        public string name { get; set; }
+
+        [Required]
+        [Column(TypeName = "varchar(50)")]
+        public string relationship { get; set; }
+
+        [Required]
+        [Column(TypeName = "varchar(50)")]
+        public string position { get; set; }
+
+        [Required]
+        [Column(TypeName = "varchar(100)")]
+        public string rank { get; set; }
+    }
+}
---
+++ Model/Ibp_registration.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Data;
+
+namespace ibp_portal_server.Model
+{
+    public class Ibp_registration
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        [ForeignKey("Ibp")]
+        public int ibp_id { get; set; }
+        public Ibp Ibp { get; set; }
+
+        [ForeignKey("User")]
+        public int user_id { get; set; }
+        public User User { get; set; }
+
+        [ForeignKey("Role")]
+        public int role_id { get; set; }
+        public Role Role { get; set; }
+
+        public DateTime confirmed_date { get; set; }
+    }
+}
